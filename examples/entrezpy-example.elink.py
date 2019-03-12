89ac43b289f12e213e387ae40fff6a6ddc9ab635
jpb <jan.buchmann@sydney.edu.au>
2019-03-12 12:37:22 +1100

    docs: renaming elink and esearch examples

--- examples/entrezpy-example.elink.py
+++ examples/entrezpy-example.elink.py
@@ -1,52 +1,77 @@
 #!/usr/bin/env python3
-#-------------------------------------------------------------------------------
-# \author Jan P Buchmann <jan.buchmann@sydney.edu.au>
-# \copyright 2018 The University of Sydney
-# \description Demonstrate Entrezpy's esearch functionality and setup.
-#   Elink() links UIDs between Entrez databases or reports outside links for
-# UIDs, e.g. publications linked to this UID[0].
+"""
+.. module:: entrezpy-examples.esearch
+  :synopsis:
+    Example of using entrezpy's elink function.
 
-# The examples are stored as parameters in the list `examples` (taken from [0]).
-#   Outline:
-#     0. Import entrezpy
-#     1. Create an instance of Esearcher() with the required parameters:
-#         - instance name
-#         - user email.
-#         These are required by NCBI [1]. The instance name corresponds to the
-#         Eutils `tool` parameter [1].
-#     3. Loop over the examples, post the UIDs and return the corresponding
-#        WebEnv and QueryKey for them to later use.
-#
-# N.B.
-#   NCBI api key[1]: If an apikey is passed to Efetcher(), it will be used to
-#                    allow more requests [1]. Without apikey, Entrezpy checks if
-#                    the environmental variable $NCBI_API_KEY is set. If not,
-#                    less queries per second are performed.
-#
-#  References:
-#    [0]: https://www.ncbi.nlm.nih.gov/books/NBK25499/#chapter4.Elink
-#    [1]: https://www.ncbi.nlm.nih.gov/books/NBK25497/#chapter2.Usage_Guidelines_and_Requirement
-#    [2]: https://docs.python.org/3/library/argparse.html#module-argparse
-#-------------------------------------------------------------------------------
+.. moduleauthor:: Jan P Buchmann <jan.buchmann@sydney.edu.au>
+
+..
+  Copyright 2018, 2019 The University of Sydney
+
+  entrezpy-examples.esearch.py is free software: you can redistribute it and/or
+  modify it under the terms of the GNU Lesser General Public License as
+  published by the Free Software Foundation, either version 3 of the License,
+  or (at your option) any later version.
+
+  entrezpy-examples.elink.py is distributed in the hope that it will be
+  useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General
+  Public License for more details.
+
+  You should have received a copy of the GNU General Public License along with
+  entrezpy-examples.elink.py. If not, see <https://www.gnu.org/licenses/>.
+
+
+  Elink links UIDs within or between Entrez databases or reports links outside
+  Entrez for a set of UIDs [0].
+
+  The examples are stored as parameters in the list `examples` (modified from
+  [0]).
+
+  Outline
+  -------
+  0. Import entrezpy
+  1. Create an instance of Esearcher() with the required parameters:
+      - instance name
+      - user email.
+      These are required by NCBI [1]. The instance name corresponds to the
+      Eutils `tool` parameter [1].
+  3. Loop over the examples
+
+  N.B.
+  NCBI api key[1]: If an apikey is passed to Efetcher(), it will be used to
+  allow more requests [1]. Without apikey, Entrezpy checks if the environmental
+  variable $NCBI_API_KEY is set. If not, fewer queries per second are performed.
+
+  Setup
+  -----
+  Set the proper import path to the required classes relative to this file by
+  updating `sys.payth`. The example assumes you cloned the git repository from
+  https://gitlab.com/ncbipy/entrezpy.git.
+
+
+  $reporoot
+  |-- examples
+  |   `-- entrezpy-examples.elink.py  <-You are here
+  `-- src
+      `-- entrezpy
+          `-- elink
+              |-- elink_analyzer.py
+              `-- elink.py
+
+  References:
+    [0]: https://www.ncbi.nlm.nih.gov/books/NBK25499/#chapter4.Elink
+    [1]: https://www.ncbi.nlm.nih.gov/books/NBK25497/#chapter2.Usage_Guidelines_and_Requirement
+    [2]: https://docs.python.org/3/library/argparse.html#module-argparse
+"""
+
 
 import os
 import sys
 import json
 import time
 import argparse
-
-""" Setup Entrezpy
-Set the proper import path to the required classes relative to this file by
-updating sys.payth. The example assumes you cloned the git repository.
-$reporoot
-|-- examples
-|   `-- entrezpy.epost-examples.py  <-You are here
-`-- src
-    `-- entrezpy
-        `-- elink
-            |-- elink_analyzer.py
-            `-- elinker.py
-"""
 
 sys.path.insert(1, os.path.join(sys.path[0], '../src'))
 import entrezpy.elink.elinker
--- examples/entrezpy-example.esearch.py
+++ examples/entrezpy-example.esearch.py
@@ -1,74 +1,80 @@
 #!/usr/bin/env python3
 """
-..
-  Copyright 2018, 2019 The University of Sydney
-  This file is part of entrezpy.
-
-  Entrezpy is free software: you can redistribute it and/or modify it under the
-  terms of the GNU Lesser General Public License as published by the Free
-  Software Foundation, either version 3 of the License, or (at your option) any
-  later version.
-
-  Entrezpy is distributed in the hope that it will be useful, but WITHOUT ANY
-  WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
-  A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
-
-  You should have received a copy of the GNU General Public License
-  along with entrezpy.  If not, see <https://www.gnu.org/licenses/>.
-
-.. module:: entrezpy.esearch.esearch_parameter
+.. module:: entrezpy-examples.esearch
   :synopsis:
-    Demonstrate Entrezpy's esearch functionality and setup. Esearch() searches
-    Entrez databases and returns corresponding UIDs or WebEnv/Querykey
-    references.
+    Example of using entrezpy's esearch function.
 
 .. moduleauthor:: Jan P Buchmann <jan.buchmann@sydney.edu.au>
 
 ..
-  The examples are stored as parameters in the list `examples` (taken from [0]).
-    Outline:
-      0. Import entrezpy
-      1. Create an instance of Esearcher() with the required parameters:
-          - instance name
-          - user email.
-          These are required by NCBI [1]. The instance name corresponds to the
-          Eutils `tool` parameter [1].
-      3. Loop over the examples, post the UIDs and return the corresponding
-        WebEnv and QueryKey for them to later use.
+  Copyright 2018, 2019 The University of Sydney
+
+  entrezpy-examples.esearch.py is free software: you can redistribute it and/or
+  modify it under the terms of the GNU Lesser General Public License as
+  published by the Free Software Foundation, either version 3 of the License,
+  or (at your option) any later version.
+
+  entrezpy-examples.esearch.py is distributed in the hope that it will be
+  useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General
+  Public License for more details.
+
+  You should have received a copy of the GNU General Public License along with
+  entrezpy.esearch-exmaples.py. If not, see <https://www.gnu.org/licenses/>.
+
+  The examples are stored as parameters in the list `examples` (modified  from
+  [0]).
+
+  Outline
+  -------
+  0. Import entrezpy
+
+  1. Create an instance of Esearcher() with the required parameters:
+      - instance name
+      - user email.
+      These are required by NCBI [1]. The instance name corresponds to the
+      Eutils `tool` parameter [1].
+
+  3. Loop over the examples, post the UIDs and return the corresponding WebEnv
+     and QueryKey for them to later use.
 
   N.B.
-    NCBI api key[1]: If an apikey is passed to Efetcher(), it will be used to
-                    allow more requests [1]. Without apikey, Entrezpy checks if
-                    the environmental variable $NCBI_API_KEY is set. If not,
-                    less queries per second are performed.
+  NCBI api key[1]: If an apikey is passed to Efetcher(), it will be used to
+  allow more requests [1]. Without apikey, Entrezpy checks if the environmental
+  variable $NCBI_API_KEY is set. If not, less queries per second are performed.
+
+  Setup
+  -----
+  Set the proper import path to the required classes relative to this file by
+  updating `sys.payth`. The example assumes you cloned the git repository from
+  https://gitlab.com/ncbipy/entrezpy.git.
+
+  $reporoot
+  |-- examples
+  |   `-- entrezpy-examples.esearch.py  <-You are here
+  `-- src
+      `-- entrezpy
+          `-- esearch
+              |-- esearch_analyzer.py
+              `-- esearcher.py
 
   References:
     [0]: https://www.ncbi.nlm.nih.gov/books/NBK25499/#chapter4.Esearch
     [1]: https://www.ncbi.nlm.nih.gov/books/NBK25497/#chapter2.Usage_Guidelines_and_Requirement
     [2]: https://docs.python.org/3/library/argparse.html#module-argparse
 """
+
+
 import os
 import sys
 import time
 import json
 import argparse
 
-""" Setup Entrezpy
-Set the proper import path to the required classes relative to this file by
-updating sys.payth. The example assumes you cloned the git repository.
-$reporoot
-|-- examples
-|   `-- entrezpy.epost-examples.py  <-You are here
-`-- src
-    `-- entrezpy
-        `-- esearch
-            |-- esearch_analyzer.py
-            `-- esearcher.py
-"""
-
 sys.path.insert(1, os.path.join(sys.path[0], '../src'))
 import entrezpy.esearch.esearcher
 import entrezpy.esearch.esearch_analyzer
+
 
 def main():
   ap = argparse.ArgumentParser(description="ncbipy-eutils esearch examples from \
@@ -84,16 +90,16 @@ def main():
 
   args = ap.parse_args()
   examples = [
-    #{'db':'nucleotide','term':'viruses[orgn]', 'rettype':'count'},
+    {'db':'nucleotide','term':'viruses[orgn]', 'rettype':'count'},
     #{'db':'nucleotide','term':'viruses[orgn]', 'limit': 1,},
     #{'db':'nucleotide','term':'viruses[orgn]', 'limit': 110000,},
     #{'db':'nucleotide','term':'viruses[orgn]', 'limit': 100, 'retmax' : 99, 'idtype' : 'acc'},
     #{'db':'pubmed','term':'cancer','reldate':60,'datetype':'edat','retmax':89, 'limit': 120, 'usehistory':True},
-    {'db':'pubmed','term':'PNAS[ta] AND 97[vi]', 'retstart':6, 'retmax': 6},
+    #{'db':'pubmed','term':'PNAS[ta] AND 97[vi]', 'retstart':6, 'retmax': 6},
     #{'db':'nlmcatalog','term':'obstetrics AND ncbijournals[filter]', 'retmax':20},
     #{'db':'pmc','term':'stem cells AND free fulltext[filter]'},
     #{'db':'nucleotide','term':'biomol trna', 'field':'prop', 'mindate': 1982, 'maxdate':2017}, # Empty result
-    {'db':'nucleotide','term':'biomol trna', 'field':'prop', 'sort' : 'Date Released', 'mindate': 2018, 'maxdate':2019, 'datetype' : 'pdat'},
+    #{'db':'nucleotide','term':'biomol trna', 'field':'prop', 'sort' : 'Date Released', 'mindate': 2018, 'maxdate':2019, 'datetype' : 'pdat'},
     #{'db':'protein','term':'70000:90000[molecular weight]', 'retmax':20}
     ]
 
@@ -122,7 +128,7 @@ def main():
     if not a.isSuccess():
       print("\tFailed: Response errors")
       return 0
-    print("\Response OK")
+    print("\tResponse OK")
     if a.isEmpty():
       print("+++\tWARNING: No results for example {}".format(i))
     else:
