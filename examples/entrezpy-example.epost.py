ef0c246c90ea9db4ee599168726642f5bf31e467
jpb <jan.buchmann@sydney.edu.au>
2019-03-13 14:43:05 +1100

    docs: update docstring

--- examples/entrezpy-example.epost.py
+++ examples/entrezpy-example.epost.py
@@ -1,33 +1,69 @@
 #!/usr/bin/env python3
-#-------------------------------------------------------------------------------
-# \author Jan P Buchmann <jan.buchmann@sydney.edu.au>
-# \copyright 2018 The University of Sydney
-# \description Demonstrate Entrezpy's epost functionality and setup.
-#   Eposter() posts a UIDs to the Entrez history server and returns the
-#   corresponding WebEnv and Querykey for subsequent use [0].
-#   For
-# The examples are stored as parameters in the list `examples` (taken from [0]).
-#   Outline:
-#     0. Import entrezpy
-#     1. Create an instance of Eposter() with the required parameters:
-#         - instance name
-#         - user email.
-#         These are required by NCBI [1]. The instance name corresponds to the
-#         Eutils `tool` parameter [1].
-#     3. Loop over the examples, post the UIDs and return the corresponding
-#        WebEnv and QueryKey for them to later use.
-#
-# N.B.
-#   NCBI api key[1]: If an apikey is passed to Efetcher(), it will be used to
-#                    allow more requests [1]. Without apikey, Entrezpy checks if
-#                    the environmental variable $NCBI_API_KEY is set. If not,
-#                    less queries per second are performed.
-#
-#  References:
-#    [0]: https://www.ncbi.nlm.nih.gov/books/NBK25499/#chapter4.EPost
-#    [1]: https://www.ncbi.nlm.nih.gov/books/NBK25497/#chapter2.Usage_Guidelines_and_Requirement
-#    [2]: https://docs.python.org/3/library/argparse.html#module-argparse
-#-------------------------------------------------------------------------------
+"""
+.. module:: entrezpy-examples.esearch
+  :synopsis:
+    Example of using entrezpy's esearch function.
+
+.. moduleauthor:: Jan P Buchmann <jan.buchmann@sydney.edu.au>
+
+..
+  Copyright 2018 The University of Sydney
+
+  entrezpy-examples.esearch.py is free software: you can redistribute it and/or
+  modify it under the terms of the GNU Lesser General Public License as
+  published by the Free Software Foundation, either version 3 of the License,
+  or (at your option) any later version.
+
+  entrezpy-examples.esearch.py is distributed in the hope that it will be
+  useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General
+  Public License for more details.
+
+  You should have received a copy of the GNU General Public License along with
+  entrezpy.esearch-exmaples.py. If not, see <https://www.gnu.org/licenses/>.
+
+  The examples are stored as parameters in the list `examples` (modified  from
+  [0]).
+
+  Outline
+  -------
+  0. Import entrezpy
+
+  1. Create an Eposter instancewith the required parameters:
+      - instance name
+      - user email.
+      These are required by NCBI [1]. The instance name corresponds to the
+      Eutils `tool` parameter [1].
+
+  3. Loop over the examples, post the UIDs and return the corresponding WebEnv
+     and QueryKey for them to later use.
+
+  N.B.
+  NCBI api key[1]: If an apikey is passed as parameter, it will be used to
+  allow more requests [1]. Without apikey, Entrezpy checks if the environmental
+  variable $NCBI_API_KEY is set. If not, less queries per second are performed.
+
+  Setup
+  -----
+  Set the proper import path to the required classes relative to this file by
+  updating `sys.payth`. The example assumes you cloned the git repository from
+  https://gitlab.com/ncbipy/entrezpy.git.
+
+  $reporoot
+  |-- examples
+  |   `-- entrezpy-examples.epost.py  <-You are here
+  `-- src
+      `-- entrezpy
+          `-- esearch
+              |-- epost_analyzer.py
+              `-- epost.py
+
+  References:
+    [0]: https://www.ncbi.nlm.nih.gov/books/NBK25499/#chapter4.Epost
+    [1]: https://www.ncbi.nlm.nih.gov/books/NBK25497/#chapter2.Usage_Guidelines_and_Requirement
+    [2]: https://docs.python.org/3/library/argparse.html#module-argparse
+"""
+
 
 import os
 import sys
@@ -35,21 +71,10 @@ import time
 import json
 import argparse
 
-""" Setup Entrezpy
-Set the proper import path to the required classes relative to this file by
-updating sys.payth. The example assumes you cloned the repository.
-$reporoot
-|-- examples
-|   `-- entrezpy.epost-examples.py  <-You are here
-`-- src
-    `-- entrezpy
-        `-- epost
-            |-- epost_analyzer.py
-            `-- eposter.py
-"""
 sys.path.insert(1, os.path.join(sys.path[0], '../src'))
 import entrezpy.epost.eposter
 import entrezpy.epost.epost_analyzer
+
 
 def main():
   demo_src = 'https://www.ncbi.nlm.nih.gov/books/NBK25499/#chapter4.EPost'
@@ -93,7 +118,6 @@ def main():
   print("+Update WebEnv of query #2 with query #3")
   ep = entrezpy.epost.eposter.Eposter('eposter', args.email, args.apikey)
   new_a = ep.inquire(dict({'WebEnv':a.result.webenv}, **examples[1]))
-  #return dict(extend, **reqdump)
   print(a.result.dump(), '\n', new_a.result.dump())
   print("+Time total: {} sec".format(time.time()-start))
   return 0
