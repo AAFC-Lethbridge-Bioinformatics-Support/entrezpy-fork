eb1b779b35ff170b6720f6bc29d74db3fbd5f725
jpb <jan.buchmann@sydney.edu.au>
2019-03-13 19:30:13 +1100

    wip: adjust Wally for new version

--- src/entrezpy/wally.py
+++ src/entrezpy/wally.py
@@ -1,9 +1,26 @@
-#-------------------------------------------------------------------------------
-#  \author Jan P Buchmann <jan.buchmann@sydney.edu.au>
-#  \copyright 2018 The University of Sydney
-#  \description The Wally class facilitates the assembly of Edirect
-#                queries.
-#-------------------------------------------------------------------------------
+"""
+..
+  Copyright 2018 The University of Sydney
+  This file is part of entrezpy.
+
+  Entrezpy is free software: you can redistribute it and/or modify it under the
+  terms of the GNU Lesser General Public License as published by the Free
+  Software Foundation, either version 3 of the License, or (at your option) any
+  later version.
+
+  Entrezpy is distributed in the hope that it will be useful, but WITHOUT ANY
+  WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+  A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with entrezpy.  If not, see <https://www.gnu.org/licenses/>.
+
+.. module:: entrezpy.wally
+   :synopsis: Exports class Wally implementing entrezpy pipelines to query
+      NCBI E-Utilities
+
+.. moduleauthor:: Jan P Buchmann <jan.buchmann@sydney.edu.au>
+"""
 
 import sys
 import json
@@ -12,53 +29,59 @@ import base64
 import queue
 import logging
 
-
-from .esearch import esearcher
-from .esearch import esearch_analyzer
-from .elink import elinker
-from .elink import elink_analyzer
-from .epost import eposter
-from .epost import epost_analyzer
-from .efetch import efetcher
-from .esummary import esummarizer
-from .esummary import esummary_analyzer
+import entrezpy.esearch.esearcher
+import entrezpy.esearch.esearch_analyzer
+import entrezpy.elink.elinker
+import entrezpy.elink.elink_analyzer
+import entrezpy.epost.eposter
+import entrezpy.epost.epost_analyzer
+import entrezpy.efetch.efetcher
+import entrezpy.esummary.esummarizer
+import entrezpy.esummary.esummary_analyzer
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 logger.addHandler(logging.StreamHandler())
 
-## Wally simplifies to create pipelines and queries for entrezpy.
-# Wally stores the results from previous requests, allowing to retrieve them
-# later if required, reducing the need to redownload data within a pipeline.
+##
+#
+#
 class Wally:
+  """Wally simplifies to create pipelines and queries for entrezpy. Wally stores
+  results from previous requests, allowing to concatenate queries and
+  retrieve obtained results later if required to reduce the need to redownload
+  data. Wally can use multiple threads to speed up data download, but some
+  external libraries can break, e.g. SQLite3.
 
-  ## Static dictionaries to store results in analyzers and individual queries
-  # for pipelines in queries. Queries are visible to all instances of Wally.
-  analyzers = {}
+  :param str email: user email
+  :param str apikey: NCBI apikey
+  :param str apikey_var: enviroment variable storing NCBI apikey
+  :param int threads: set threads for multithreading
+  """
+
   queries = {}
+  """Query storage"""
+
+  analyzers = {}
+  """Analyzed query storage"""
 
   class Query:
-    """ Entrezpy query for a Wally pipeline
-      Wally assembles pipelines using several Query() instances. If a dependency
-      is given, it uses those parameters as basis using
-      `:func: resolve_dependency`.
+    """ Entrezpy query for a Wally pipeline. Wally assembles pipelines using
+    several Query() instances. If a dependency is given, it uses those
+    parameters as basis using `:meth:`.resolve_dependency`.
 
-      :param function: Eutils function
-      :type function: str
-      :param parameter: function parameters
-      :type parameter: dict
-      :param dependency: query id from earlier query
-      :type dependency: uuid4 string
-      :param analyzer: analyzer instance for this query
-      :type analyzer: instance of EutilsAnalyzer
-
+    :param str function: Eutils function
+    :param dict parameter: function parameters
+    :param str dependency: query id from earlier query
+    :param analyzer: analyzer instance for this query
+    :type analyzer: :class:`entrezpy.base.analyzer.EutilsAnalyzer`
     """
+
     def __init__(self, function, parameter, dependency=None, analyzer=None):
       if not parameter and not dependency:
-        logger.error(json.dumps({__name__:{'Error': 'Missing expected parameters' \
-                                           'parameter and/or `dependency`.',
-                                           'action' : 'abort'}}))
-        sys.exit()
+        sys.exit(logger.error(json.dumps({__name__:{'Error': 'Missing required parameters' \
+                                                    'parameter and/or `dependency`.',
+                                                    'action' : 'abort'}})))
       if not parameter:
         parameter = {}
       self.id = base64.urlsafe_b64encode(uuid.uuid4().bytes).decode()
@@ -67,11 +90,10 @@ class Wally:
       self.dependency = dependency
       self.analyzer = analyzer
 
-    ## resolve dependencies to get parameters from an earlier query
-    # If earlier requets are given as dependencies as well as new parameters, the
-    # new parameters will overwrite those from the dependency.
-    # @param[in] analyzers dictionary global Wally.analzyers dictionary
     def resolve_dependency(self):
+      """Resolves dependencies to obtain paremeters from earlier query.
+      Parameters passed to this instance will overwrite dependency parameters
+      """
       if self.dependency:
         parameter = Wally.analyzers[self.dependency].result.get_link_parameter()
         if self.function == 'elink':
@@ -79,75 +101,81 @@ class Wally:
         parameter.update(self.parameter)
         self.parameter = parameter
 
-  ## Nested class handling requets piplines. Wally will return an instance
-  # of Pipeline which can be than populated with requests. Individual requests
-  # can reuse parameters from earlier requests. If earlier requets are given as
-  # dependencies as well as new parameters, the new parameters will overwrite
-  # those from the dependency.
   class Pipeline:
-
-    ## Ctor for Pipeline
-    # store reference to global query storage in query_map and initialize queue
-    # to store query for current pipeline
+    """The Pipeline class implements a query pipeline with several consecutive
+    queries. New pipelines are obtained through :class:`.Wally`. They store
+    queries which will get resolved in the order added and can reuase parameters
+    from earlier queries.
+    """
 
     def __init__(self, query_map):
+      """:ivar dict query_map: :attr:`Wally.queries`
+      :ivar  queries: queries for this instance
+      :type  queries: :class:`queue.Queue()`
+      """
       self.query_map = query_map
       self.queries = queue.Queue()
 
-    ## add search, link, post and sumary queries to pipeline
-    # @param[in] parameter dictionary with entrez parameters for search
-    # @param[in] dependency query id for earlier search
-    # @param[in] analyzer analyzer instance to use for this search
-    # @param[out] WallyQuery() instance
     def add_search(self, parameter=None, dependency=None, analyzer=None):
+      """Adds Esearch query
+
+      :param dict parameter: Esearch E-Eutility parameters
+      :param str dependency: query id from earlier query
+      :param analyzer: analyzer for this query
+      :type  analyzer: :class:`entrezpy.base.analyzer.EutilsAnalyzer`
+      :return: Wally query
+      :rtype: :class:`WallyQuery`
+      """
       return self.add_query(Wally.Query('esearch', parameter, dependency, analyzer))
 
-    ## add link query. Same options as add_search()
     def add_link(self, parameter=None, dependency=None, analyzer=None):
+      """Adds Elink query. Signature as :meth:`Wally.Pipeline.add_search`"""
       return self.add_query(Wally.Query('elink', parameter, dependency, analyzer))
 
-    ## add post query. Same options as add_search()
     def add_post(self, parameter=None, dependency=None, analyzer=None):
+      """Adds Epost query. Signature as :meth:`Wally.Pipeline.add_search`"""
       return self.add_query(Wally.Query('epost', parameter, dependency, analyzer))
 
-    ## add summary query. Same options as add_search()
     def add_summary(self, parameter=None, dependency=None, analyzer=None):
+      """Adds Esummary query. Signature as :meth:`Wally.Pipeline.add_search`"""
       return self.add_query(Wally.Query('esummary', parameter, dependency, analyzer))
 
-    ## add fetch query to pipeline. Same options as add_search(), but
-    # requires an analyzer
     def add_fetch(self, parameter=None, dependency=None, analyzer=None):
+      """Adds Efetch query. Same signature as :meth:`Wally.Pipeline.add_search`
+      but analyzer is required.
+      """
       if not analyzer:
         logger.error(json.dumps({__name__ : {'Error' :
                                              {'Missing required parameter' : 'analyzer',
                                               'action' : 'abort'}}}))
       return self.add_query(Wally.Query('efetch', parameter, dependency, analyzer))
 
-    ## add query to global and local storage
-    # @param[in] query WallyQuery()
-    # @param[out] query.id, uuid4() query id for added query
     def add_query(self, query):
+      """Adds query to own pipeline and storage
+
+      :param query: Wally query
+      :type  query: :class:`Wally.Query`
+      :return: query id of added query
+      :rtype: str
+      """
       self.queries.put(query.id)
       self.query_map[query.id] = query
       return query.id
 
-  ## Ctor for Wally
-  # Wally can use multiple threads to speed up data download, but some external
-  # libraries used within analyzers can break, e.g. SQLite3
-  # @param[in] email email address as requirs by NCBI Eutils
-  # @param[in] apikey NCBI Apikey
-  # @param[in] threads int  Number of threads to use for data download.
-  def __init__(self, email, apikey=None, threads=None):
+  def __init__(self, email, apikey=None, apikey_envar=None, threads=None):
     self.tool = 'wally'
     self.email = email
     self.apikey = apikey
+    self.api_envar = apikey_envar
     self.threads = threads
 
-  ## run the queries within the local pipeline.
-  # Individual queries are recognized and a corrsponding entrezpy method
-  # invoked. If errors are encountered, it aborts.
-  # param[in] Wally.Pipeline() Wally pipeline instance with queries in queue.
   def run(self, qry):
+    """Runs one query in pipeline and checks for errors. If errors are
+    encounterd the pipeline is aborted.
+
+    :param qry: Wally query
+    :type  qry: :class:`Wally.Query`
+    """
     while not qry.queries.empty():
       q = Wally.queries[qry.queries.get()]
       q.resolve_dependency()
@@ -166,55 +194,116 @@ class Wally:
       self.check_query(q)
     return Wally.analyzers[q.id]
 
-  ## Check if an individual query in a pipeline has been succesful.
-  # Aborts if errors are encountered
-  # @param[in] WallyQuery()
   def check_query(self, query):
+    """Check for succesful query in a pipeline.
+
+    :param query: Wally query
+    :type  query: :class:`Wally.Query`
+    """
     if not Wally.analyzers[query.id]:
       sys.exit("Request errors in query {}".format(query.id))
     if not Wally.analyzers[query.id].isSuccess():
-      logger.info(json.dumps({__name__ : {'response error': {'query_id' : query.id,
-                                                             'action' : 'abort'}}}))
-      sys.exit()
+      sys.exit(logger.info(json.dumps({__name__ : {'response error': {'query_id' : query.id,
+                                                                      'action' : 'abort'}}})))
 
-  ## return a stored result from a previous run.
-  # @param[in] query_id, uuid.uuid4() query id
-  # @param[out] result, result instance or None if not found
   def get_result(self, query_id):
+    """"Returns stored result from previus run.
+
+    :param str query_id: query id
+    :return: Result from this query
+    :rtype: :class:`entrezpy.base.result.EutilsResult`
+    """
     r = self.analyzers.get(query_id)
     if not r:
       return None
     return r.result
 
-  ## get new instance of Wally.Pipeline()
   def new_pipeline(self):
+    """Retrurns new Wally pipeline.
+
+    :return: Wally pipeline
+    :rtype: :class:`Wally.Pipeline`
+    """
     return self.Pipeline(Wally.queries)
 
-  ## Configure and run a entrepy queries
-  # Analyzers are passed as class and instantiated later. Ensures new instances
-  # for every query
-  # @param[in] query WallyQuery()
-  # @param[in] analzyer analyzer class
-  def search(self, query, analyzer=esearch_analyzer.EsearchAnalyzer):
-    analyzer = query.analyzer if query.analyzer else analyzer()
-    return esearcher.Esearcher(self.tool, self.email, self.apikey, threads=self.threads,
-                               id=query.id).inquire(query.parameter, analyzer)
+  def search(self, query, analyzer=entrezpy.esearch.esearch_analyzer.EsearchAnalyzer):
+    """Configures and runs an Esearch query. Analyzer are class references and
+    instantiated here.
 
-  def summarize(self, query, analyzer=esummary_analyzer.EsummaryAnalzyer):
+    :param query: Wally Query
+    :type  query: :class:`Wally.Query`
+    :param analyzer: reference to analyzer class
+    :return: analyzer
+    :rtype: :class:`entrezpy.esearch.esearch_analyzer.EsearchAnalyzer`
+    """
     analyzer = query.analyzer if query.analyzer else analyzer()
-    return esummarizer.Esummarizer(self.tool, self.email, self.apikey, threads=self.threads,
-                                   id=query.id).inquire(query.parameter, analyzer)
+    return entrezpy.esearch.esearcher.Esearcher(self.tool,
+                                                self.email,
+                                                self.apikey,
+                                                threads=self.threads,
+                                                qid=query.id).inquire(query.parameter, analyzer)
 
-  def link(self, query, analyzer=elink_analyzer.ElinkAnalyzer):
-    analyzer = query.analyzer if query.analyzer else analyzer()
-    return elinker.Elinker(self.tool, self.email, self.apikey, threads=self.threads,
-                           id=query.id).inquire(query.parameter, analyzer)
+  def summarize(self, query, analyzer=entrezpy.esummary.esummary_analyzer.EsummaryAnalzyer):
+    """Configures and runs an Esummary query. Analyzer are class references and
+    instantiated here.
 
-  def post(self, query, analyzer=epost_analyzer.EpostAnalyzer):
+    :param query: Wally Query
+    :type  query: :class:`Wally.Query`
+    :param analyzer: reference to analyzer class
+    :return: analyzer
+    :rtype: :class:`entrezpy.esummary.esummary_analyzer.EsummaryAnalyzer`
+    """
     analyzer = query.analyzer if query.analyzer else analyzer()
-    return eposter.Eposter(self.tool, self.email, self.apikey, threads=self.threads,
-                           id=query.id).inquire(query.parameter, analyzer)
+    return entrezpy.esummary.esummarizer.Esummarizer(self.tool,
+                                                     self.email,
+                                                     self.apikey,
+                                                     threads=self.threads,
+                                                     id=query.id).inquire(query.parameter, analyzer)
+
+  def link(self, query, analyzer=entrezpy.elink.elink_analyzer.ElinkAnalyzer):
+    """Configures and runs an Elink query. Analyzer are class references and
+    instantiated here.
+
+    :param query: Wally Query
+    :type  query: :class:`Wally.Query`
+    :param analyzer: reference to analyzer class
+    :return: analyzer
+    :rtype: :class:`entrezpy.elink.elink_analyzer.ElinkAnalyzer`
+    """
+    analyzer = query.analyzer if query.analyzer else analyzer()
+    return entrezpy.elink.elinker.Elinker(self.tool,
+                                          self.email,
+                                          self.apikey,
+                                          threads=self.threads,
+                                          qid=query.id).inquire(query.parameter, analyzer)
+
+  def post(self, query, analyzer=entrezpy.epost.epost_analyzer.EpostAnalyzer):
+    """Configures and runs an Epost query. Analyzer are class references and
+    instantiated here.
+
+    :param query: Wally Query
+    :type  query: :class:`Wally.Query`
+    :param analyzer: reference to analyzer class
+    :return: analyzer
+    :rtype: :class:`entrezpy.epost.epost_analyzer.EpostAnalyzer`
+    """
+    analyzer = query.analyzer if query.analyzer else analyzer()
+    return entrezpy.epost.eposter.Eposter(self.tool,
+                                          self.email,
+                                          self.apikey,
+                                          threads=self.threads,
+                                          qid=query.id).inquire(query.parameter, analyzer)
 
   def fetch(self, query):
-    return efetcher.Efetcher(self.tool, self.email, self.apikey, threads=self.threads,
-                             id=query.id).inquire(query.parameter, query.analyzer)
+    """uns an Efetch query. The Analyzer needs to be added to the quuery
+
+    :param query: Wally Query
+    :type  query: :class:`Wally.Query`
+    :return: analyzer
+    :rtype: :class:`entrezpy.efetch.efetch_analyzer.EfetchAnalyzer`
+    """
+    return entrezpy.efetch.efetcher.Efetcher(self.tool,
+                                             self.email,
+                                             self.apikey,
+                                             threads=self.threads,
+                                             qid=query.id).inquire(query.parameter, query.analyzer)
