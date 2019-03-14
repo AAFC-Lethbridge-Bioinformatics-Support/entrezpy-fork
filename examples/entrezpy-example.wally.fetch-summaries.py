ebe72e9e0d91bfa1d84e3f7ab1fcddb32ab759f5
jpb <jan.buchmann@sydney.edu.au>
2019-03-14 23:44:08 +1100

    style: add docstring headers to Wllay examples

--- examples/entrezpy-example.wally.fetch-summaries.py
+++ examples/entrezpy-example.wally.fetch-summaries.py
@@ -1,8 +1,64 @@
 #!/usr/bin/env python3
-#-------------------------------------------------------------------------------
-#  \author Jan P Buchmann <jan.buchmann@sydney.edu.au>
-#  \copyright 2018 The University of Sydney
-#-------------------------------------------------------------------------------
+"""
+.. module:: entrezpy-example.wally.fetch-summaries
+  :synopsis:
+    Example of using entrezpy's Wally to run a summary pipeline
+
+.. moduleauthor:: Jan P Buchmann <jan.buchmann@sydney.edu.au>
+
+..
+  Copyright 2018 The University of Sydney
+
+  Demonstrate Entrezpy's Wally class and setup. Wally facilitates the
+  creation of E-Utility query pipelines [0].
+
+  The examples are stored as parameters in the list `examples` (taken from [0]).
+  Outline
+  -------
+  0. Import entrezpy
+  1. Create a Wally instance  with the required parameters:
+      - instance name
+      - user email.
+      These are required by NCBI [1]. The instance name corresponds to the
+      Eutils `tool` parameter [1].
+  2. Get a new Wally pipeline
+  3. Add queries to the pipline
+  4. Run the pipeline
+  3. Print specific attributes form the summaries to STDOUT
+
+  Setup
+  -----
+  Set the proper import path to the required classes relative to this file by
+  updating ``sys.payth``. The example assumes you cloned the git repository from
+  https://gitlab.com/ncbipy/entrezpy.git.
+
+::
+  $reporoot
+  |-- examples
+  |   `-- entrezpy-examples.wally.fetch-summaries.py  <-You are here
+  `-- src
+      `-- entrezpy
+          `-- efetch
+              |-- efetch_analyzer.py
+              `-- efetch.py
+
+  N.B.
+    NCBI api key[1]: If an apikey is passed to Efetcher(), it will be used to
+                    allow more requests [1]. Without apikey, Entrezpy checks if
+                    the environmental variable $NCBI_API_KEY is set. If not,
+                    less queries per second are performed.
+
+    Efetch analyzer:  Efecth is unique in respect to all other functions as its
+                      function `inquire` requires an analyzer as parameter. This
+                      example uses the default analyzer which prints either the
+                      errors or stores the result as string to
+                      `analyzer.result`. It can be easly adapted for more
+                      complex tasks (see documentation).
+  References:
+    [0]: https://www.ncbi.nlm.nih.gov/books/NBK25499/#chapter4.EFetch
+    [1]: https://www.ncbi.nlm.nih.gov/books/NBK25497/#chapter2.Usage_Guidelines_and_Requirement
+    [2]: https://docs.python.org/3/library/argparse.html#module-argparse
+"""
 
 import os
 import sys
