a0f2ae1c59cd4307798f5e613b58d3b98a63ebe0
Benny Zhao <benny_zhao@sfu.ca>
2022-04-05 09:20:29 -0700

    Increase request timeouts

--- src/entrezpy/requester/requester.py
+++ src/entrezpy/requester/requester.py
@@ -52,7 +52,7 @@ class Requester:
   :param int timeout_steps: increase value for timeout errors
   """
 
-  def __init__(self, wait, max_retries=20, init_timeout=60, timeout_max=120, timeout_step=5):
+  def __init__(self, wait, max_retries=20, init_timeout=90, timeout_max=180, timeout_step=5):
     self.wait = wait
     self.max_retries = max_retries
     self.init_timeout = init_timeout
